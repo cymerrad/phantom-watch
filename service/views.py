c345fd3e853d1d355a7be2052c43c91e565a32a1
cymerrad <cymerrad+git@gmail.com>
2018-04-30 14:31:49 +0200

    WIP: idiomatic Django

--- service/views.py
+++ service/views.py
@@ -10,13 +10,14 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from django.http import Http404
-
+from rest_framework import mixins
+from rest_framework import generics
 
 class UserViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows users to be viewed or edited.
     """
-    queryset = User.objects.all().order_by('-date_joined')
+    queryset = User.objects.all()
     serializer_class = UserSerializer
 
 
@@ -27,46 +28,36 @@ class GroupViewSet(viewsets.ModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
 
-class WebPageList(APIView):
+class WebPageList(mixins.ListModelMixin,
+                  mixins.CreateModelMixin,
+                  generics.GenericAPIView):
     """
-    List all WebPages, or create a new WebPage.
+    List all Webpages, or create a new Webpage.
     """
-    def get(self, request, format=None):
-        webPages = WebPage.objects.all()
-        serializer = WebPageSerializer(webPages, many=True)
-        return Response(serializer.data)
+    queryset = WebPage.objects.all()
+    serializer_class = WebPageSerializer
 
-    def post(self, request, format=None):
-        serializer = WebPageSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
 
-class WebPageDetail(APIView):
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+class WebPageDetail(mixins.RetrieveModelMixin,
+                    mixins.UpdateModelMixin,
+                    mixins.DestroyModelMixin,
+                    generics.GenericAPIView):
     """
-    Retrieve, update or delete a WebPage.
+    Retrieve, update or delete a Webpage.
     """
-    def get_object(self, pk):
-        try:
-            return WebPage.objects.get(pk=pk)
-        except WebPage.DoesNotExist:
-            raise Http404
-            
-    def get(self, request, pk, format=None):
-        webpage = self.get_object(pk)
-        serializer = WebPageSerializer(webpage)
-        return Response(serializer.data)
+    queryset = WebPage.objects.all()
+    serializer_class = WebPageSerializer
 
-    def put(self, request, pk, format=None):
-        webpage = self.get_object(pk)
-        serializer = WebPageSerializer(webpage, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
 
-    def delete(self, request, pk, format=None):
-        webpage = self.get_object(pk)
-        webpage.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
