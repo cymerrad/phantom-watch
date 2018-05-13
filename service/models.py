725b1b9b70b5bd55916c06f45ba29baf4201efe8
Radosław Cymer <cymerrad@gmail.com>
2018-05-13 18:16:47 +0200

    Parsing crontab entries

--- service/models.py
+++ service/models.py
@@ -2,36 +2,47 @@ from django.db import models
 from django.contrib.auth.models import User
 import uuid
 from celery.schedules import crontab
-from django_celery_beat.models import PeriodicTask, IntervalSchedule
+from django_celery_beat.models import PeriodicTask, CrontabSchedule
 import json
 from datetime import datetime, timedelta
+from croniter import croniter
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
 
+def validate_crontab(ctab: str):
+    if not croniter.is_valid(ctab):
+        raise ValidationError(
+            _('%(value)s is not a valid UNIX crontab entry'),
+            params={'value': value},
+        )
+    
+def parse_crontab(ctab: str):
+    spl = ctab.split(' ')
+    return {'minute':spl[0], 'hour':spl[1], 'day_of_month':spl[2], 'month_of_year':spl[3], 'day_of_week':spl[4]}
 
-# Create your models here.
 class WebpageOrder(models.Model):
     # id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     created = models.DateTimeField(auto_now_add=True)
     url = models.CharField(max_length=2083, blank=False)
     owner = models.ForeignKey('auth.User', related_name='orders', on_delete=models.CASCADE)
-    crontab = models.CharField(max_length=50, blank=False)
+    crontab = models.CharField(max_length=50, blank=False, validators=[validate_crontab])
 
     def save(self, *args, **kwargs):
         """
         When created/updated create/update an order in Celery's crontab
         """
-        schedule, created = IntervalSchedule.objects.get_or_create(
-            every=30,
-            period=IntervalSchedule.SECONDS,
+
+        parsed = parse_crontab(self.crontab)
+        schedule, _ = CrontabSchedule.objects.get_or_create(
+            **parsed
         )
         PeriodicTask.objects.create(
-            interval=schedule,
+            crontab=schedule,
             name='Test',
             task='daemon.tasks.test_task',
             args=json.dumps(['arg1', 'arg2']),
-            kwargs=json.dumps({
-            'be_careful': True,
-            }),
-            expires=datetime.utcnow() + timedelta(minutes=3)
+            kwargs=json.dumps(parsed),
+            expires=datetime.utcnow() + timedelta(minutes=5)
         )
 
         super(WebpageOrder, self).save(*args, **kwargs)
