e7f8acf7da32c1b83844b2a2788a8c5277df9575
Radosław Cymer <cymerrad@gmail.com>
2018-05-15 13:23:45 +0200

    I can already feel the ansbile script coming... :(

---
+++ install.sh
@@ -0,0 +1,29 @@
+sudo apt install -y virtualenv python3-dev build-essential
+virtualenv -p python3 env
+source env/bin/activate
+
+sudo apt install -y libmysqlclient-dev mysql-client
+pip install -r requirements.txt
+
+sudo apt --fix-broken install -y
+
+cd mysql;
+echo "MySQL in [D]ocker or [r]egular?"
+select yn in "Docker" "Regular"; do
+    case $yn in
+        [Dd]* ) ./docker-setup.sh; break;;
+        [Rr]* ) ./mysql-setup.sh; break;;
+    esac
+done
+
+curl -L http://packages.erlang-solutions.com/site/esl/esl-erlang/FLAVOUR_1_general/esl-erlang_20.3-1~ubuntu~bionic_amd64.deb -o /tmp/esl-erlang.deb
+curl -L https://github.com/rabbitmq/rabbitmq-server/releases/download/v3.7.5/rabbitmq-server_3.7.5-1_all.deb -o /tmp/rabbitmq.deb
+sudo dpkg -i /tmp/esl-erlang.deb
+sudo dpkg -i /tmp/rabbitmq.deb
+curl -L https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2 -o /tmp/phantomjs.tar.bz2
+tar -xf /tmp/phantomjs.tar.bz2 -C daemon/webscreenshot/
+
+python manage.py makemigrations service
+python manage.py makemigrations daemon
+python manage.py migrate service
+python manage.py migrate --run-syncdb
