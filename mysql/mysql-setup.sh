2149211a65ce21bc2e23b753bdf92220aa409e18
Radosław Cymer <cymerrad@gmail.com>
2018-05-15 13:55:05 +0200

    eh

--- mysql/mysql-setup.sh
+++ mysql/mysql-setup.sh
@@ -1,6 +1,6 @@
 sudo apt install -y mysql-server-5.7 && \
 sudo mysql_secure_installation && \
-mysql -h 127.0.0.1 -u root -p < mysql/init.sql
+mysql -h 127.0.0.1 -u root -p < init.sql
 
 sudo sed -i 's/127\.0\.0\.1/0\.0\.0\.0/g' /etc/mysql/my.cnf
 mysql -uroot -p -e 'USE mysql; UPDATE `user` SET `Host`="%" WHERE `User`="root" AND `Host`="localhost"; DELETE FROM `user` WHERE `Host` != "%" AND `User`="root"; FLUSH PRIVILEGES;'
