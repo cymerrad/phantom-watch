761b08deaa56fbab3027a3cdd57391d28f30c046
Radosław Cymer <cymerrad@gmail.com>
2018-05-16 09:24:34 +0000

    apache2 host file, prod settings

--- phantom_watch/settings.py
+++ phantom_watch/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '&)f-%)6g8i@7jelg#1@+#=x5&5lx8_!bm89nw#!^#l^qs$5wdz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '127.0.1.1', '10.12.3.238']
 
 
 # Application definition
@@ -133,6 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'uploaded_media')
 MEDIA_URL = '/media/'
