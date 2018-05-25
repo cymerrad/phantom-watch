928912f56fad671df6d024cfd4a37647ef3c1a23
Radosław Cymer <cymerrad@gmail.com>
2018-05-25 21:38:22 +0200

    FORCED API ROOT VIEW

--- daemon/urls.py
+++ daemon/urls.py
@@ -2,13 +2,25 @@ from django.conf.urls import url, include
 from daemon import views as views_d
 from rest_framework.urlpatterns import format_suffix_patterns
 from rest_framework import routers
+from collections import OrderedDict
 
 app_name='daemon'
-router = routers.DefaultRouter()
-router.register(r'screenshots', views_d.ScreenshotViewSet)
 
-# TODO
-# force webpages into root api
+class ExtendableRouter(routers.DefaultRouter):
+    def get_api_root_view(self, api_urls=None):
+        api_root_dict = OrderedDict()
+        list_name = self.routes[0].name
+        for prefix, viewset, basename in self.registry:
+            api_root_dict[prefix] = list_name.format(basename=basename)
+
+        # manualy add API's here
+        api_root_dict['webpages'] = 'webpage-list'
+
+        return self.APIRootView.as_view(api_root_dict=api_root_dict)
+
+
+router = ExtendableRouter()
+router.register(r'screenshots', views_d.ScreenshotViewSet)
 
 urlpatterns = [
     url(r'^webpages/$', views_d.WebpageList.as_view(), name='webpage-list'),
