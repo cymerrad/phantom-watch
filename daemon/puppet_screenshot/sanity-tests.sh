b02411af28d7b6647e2c45b21497a46f34a37f9b
Radosław Cymer <cymerrad@gmail.com>
2018-05-25 12:28:57 +0200

    btw, http:/example.com is correct according to nodejs's url package, TIL

--- daemon/puppet_screenshot/sanity-tests.sh
+++ daemon/puppet_screenshot/sanity-tests.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 echo "expected: 2 .png's in directory screenshot"
-nodejs headless.js -d screenshots http:/pepper.pl http://banggood.com --whole
+nodejs headless.js -d screenshots http://pepper.pl http://banggood.com --whole
 
 echo "expected: 2 new directories in screenshots, in each directory multiple .png's"
-nodejs headless.js -d screenshots http:/pepper.pl http://banggood.com
+nodejs headless.js -d screenshots http://pepper.pl http://banggood.com
 
 echo "expected: 1 .jpg file named example.jpg in directory screenshots"
-nodejs headless.js -d screenshots -o example.jpg http:/pepper.pl --whole
+nodejs headless.js -d screenshots -o example.jpg http://pepper.pl --whole
 
 echo "expected: 1 new directory in screenshots, multiple .jpg's inside"
-nodejs headless.js -d screenshots -o example.jpg http:/pepper.pl
+nodejs headless.js -d screenshots -o example.jpg http://pepper.pl
 
 echo "expected: error"
 nodejs headless.js -d screenshots -o example.jpg http://pepper.pl http://alibaba.com --whole
