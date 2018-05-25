4f4b8515ce4824a44818538712cf17ebc8f348ba
Radosław Cymer <cymerrad@gmail.com>
2018-05-25 14:10:08 +0200

    WIP: parsing the result and saving it to db

--- daemon/tasks.py
+++ daemon/tasks.py
@@ -6,7 +6,7 @@ from uuid import uuid4
 from django.conf import settings
 from django.core.files.images import ImageFile as DjangoImage
 from django.core.files.uploadedfile import UploadedFile
-from daemon.webscreenshot import single_screenshot
+from daemon.puppet_screenshot.screenshot import with_timeout
 import datetime
 import logging
 import os
@@ -15,30 +15,31 @@ logger = logging.getLogger('django')
 
 @shared_task
 def take_screenshot(webpage_url, webpage_order_id, **kwargs):
-    output_filename = os.path.join(settings.SCREENSHOTS_DIRECTORY, ('%s.png' % uuid4()))
-
-    # constants in code (Y)
-    timeout = 30
+    output_filename = os.path.join('{}.png'.format(uuid4()))
+    timeout = settings.SCREENSHOT_TIMEOUT
 
     try:
-        result = single_screenshot.with_timeout(output_filename, webpage_url, timeout)
+        result = with_timeout(output_filename, webpage_url, timeout)
     except Exception as e:
         logger.error("Screenshot script raised an exception: {}".format(e))
+        return e
 
-    if result is single_screenshot.SHELL_EXECUTION_OK:    
-        ### SAVE TO DB
-        try:
-            webpage_order = WebpageOrder.objects.get(id=webpage_order_id)
+    ### SAVE TO DB
+    try:
+        # TODO
+        # PARSING THE RESULT
 
-            with open(output_filename, "rb") as fp:
-                wrapped_file = UploadedFile(fp)
-                pic = Picture(pic=wrapped_file, order=webpage_order, original_filename=output_filename,
-                    description="Screenshot of {} from {}".format(webpage_url, datetime.datetime.now()))
-                pic.save()
-                return 0
+        webpage_order = WebpageOrder.objects.get(id=webpage_order_id)
 
-        except Exception as e:
-            logger.error("Reading/saving the screenshot: {}".format(e))
+        with open(output_filename, "rb") as fp:
+            wrapped_file = UploadedFile(fp)
+            pic = Picture(pic=wrapped_file, order=webpage_order, original_filename=output_filename,
+                description="Screenshot of {} from {}".format(webpage_url, datetime.datetime.now()))
+            pic.save()
+            return 0
+
+    except Exception as e:
+        logger.error("Reading/saving the screenshot: {}".format(e))
 
     # otherwise screenshot failed
     # maybe you should do something about it?
